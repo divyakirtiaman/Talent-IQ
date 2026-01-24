31bbc3a9bf9ce61ec2a817bade7b994942723ce9
divyakirtiaman <amankumarsah95700@gmail.com>
2026-01-24 15:25:36 +0530

    small changes

---
+++ Backend/src/lib/inngest.js
@@ -0,0 +1,42 @@
+import { Inngest } from "inngest";
+import { connectDB } from "./db.js";
+import User from "../models/User.js";
+
+export const inngest = new Inngest({ id: "talent-iq" });
+
+const syncUser = inngest.createFunction(
+  { id: "sync-user" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    await connectDB();
+
+    console.log("Clerk event:", event.data); // DEBUG
+
+    const { id, email_addresses, first_name, image_url } = event.data;
+
+    const user = new User({
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      name: first_name,
+      profileImage: image_url,
+    });
+
+    await user.save();
+    console.log("User saved in MongoDB ✅");
+  }
+);
+
+const DeleteUserFromDB = inngest.createFunction(
+  { id: "delete-user" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    await connectDB();
+
+    const { id } = event.data;
+    await User.findOneAndDelete({ clerkId: id });
+
+    console.log("User deleted from MongoDB ✅");
+  }
+);
+
+export const functions = [syncUser, DeleteUserFromDB];
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,12 +1,40 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
+import { inngest, functions } from "./lib/inngest.js";
 
-const app=express();
-console.log(ENV.PORT);
-console.log(ENV.DB_URL);
- app.get("/aman",(req,res)=>{
-    res.status(200).json({msg: "api is up and runing"});
+const app = express();
+const __dirname = path.resolve();
 
- });
- app.listen(3000, ()=> 
-    console.log("Server is runing on port ENV.PORT"));
+app.use(express.json());
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+app.get("/aman", (req, res) => {
+  res.status(200).json({ msg: "api is up and running" });
+});
+
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.use((req, res) => {
+    res.sendFile(
+      path.join(__dirname, "../frontend", "dist", "index.html")
+    );
+  });
+}
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () =>
+      console.log("Server running on port", ENV.PORT)
+    );
+  } catch (err) {
+    console.log("Error while starting server", err);
+    process.exit(1);
+  }
+};
+
+startServer();

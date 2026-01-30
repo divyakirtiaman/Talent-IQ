1f7810a987e8ae19d0ad0b17114e9fb679f419e1
divyakirtiaman <amankumarsah95700@gmail.com>
2026-01-30 09:34:00 +0530

    changes auth and sunc

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,25 +1,19 @@
-<<<<<<< HEAD
+
 
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
 import viteLogo from '/vite.svg'
-import './App.css'
-=======
-import "./App.css";
-import { SignedIn, SignedOut, SignInButton, SignOutButton } from '@clerk/clerk-react'
->>>>>>> bc6eac09e8608a27daa363df96b6f60afab6bb0f
+import './index.css'
 
 
 import { SignedIn, SignedOut, SignInButton, SignOutButton } from '@clerk/clerk-react'
-
-
 function App() {
   return (
     <>
-<<<<<<< HEAD
+
 
       
-=======
+
       {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,24 +22,23 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div> */}
->>>>>>> bc6eac09e8608a27daa363df96b6f60afab6bb0f
       <h1>welcome to the app</h1>
       <SignedOut>
         <SignInButton  mood="model"/>
       </SignedOut>
-<<<<<<< HEAD
+
       <button>LogIn</button>
-=======
->>>>>>> bc6eac09e8608a27daa363df96b6f60afab6bb0f
+
+
 
       <SignedIn>
         <SignOutButton/>
       </SignedIn>
       
-<<<<<<< HEAD
 
-=======
->>>>>>> bc6eac09e8608a27daa363df96b6f60afab6bb0f
+
+
+
     </>
   )
 }
